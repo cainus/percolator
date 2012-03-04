48e35a30adefe623ad84e0ca157976356bc1b0bb
Sam DeCesare <sdeces@gmail.com>
2012-03-03 23:23:54 -0800

    extracted validator from resource, still need to return multiple errors

--- test_fixtures/resources/cars.js
+++ test_fixtures/resources/cars.js
@@ -8,18 +8,19 @@ var resource = new Resource(app, 'cars')
 var requiredFields = ['make', 'model', 'year'];
 var optionalFields = ['topSpeed'];
 
+resource.validator.setDocumentValidator(requiredFields, optionalFields);
 
-resource.setDocumentValidator(requiredFields, optionalFields);
-
-resource.add_field_validator('year', function(value){
+resource.validator.addFieldValidator('year', function(value){
   return value > 1930
 }, 'is later than 1930');
 
 resource.collectionPOST = function(req, res){ 
 	var doc = req.jsonBody;
-  if (this.validate(res, doc)) {
-  	res.send({}, 200)
-  }
+  resource.preCreate(req, res, function(err, doc){
+    if (!err) {
+      res.send({}, 200)
+    }
+  })
 }
 
 exports.handler = resource;
