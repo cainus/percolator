362d05523d10b4b2afc64dcba25b3a3fe7830dd3
cainus <gregg@caines.ca>
2012-08-19 11:33:34 -0700

    fixed bug in internal server error reporting.

--- percolator.js
+++ percolator.js
@@ -168,7 +168,7 @@ Percolator.prototype.assignErrorHandlers = function(){
     console.log(ex);
     console.log(req.method, ' ', req.url);
     console.log(ex.stack);
-    statusman.createResponder(req, res).internalServer();
+    statusman.createResponder(req, res).internalServerError();
   };
 
 
