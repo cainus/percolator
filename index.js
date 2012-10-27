4b6e02bfd4dcbdb92c9ff1a437ebe6fd64936334
cainus <gregg@caines.ca>
2012-10-27 09:50:07 -0700

    changhed UriUtil to be more of a context helper.

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ exports.StatusManager = require(dir + 'StatusManager');
 exports.HyperJson = require(dir + 'HyperJson');
 exports.HyperJsonCollection = require(dir + 'HyperJsonCollection');
 exports.JsonModule = require(dir + 'JsonModule');
-exports.UriUtil = require(dir + 'UriUtil');
 
 exports.AuthenticateContextHelper = require(helperDir + 'Authenticate');
 exports.JsonContextHelper = require(helperDir + 'Json');
 exports.BodyContextHelper = require(helperDir + 'Body');
 exports.FetchContextHelper = require(helperDir + 'Fetch');
+exports.UriContextHelper = require(helperDir + 'Uri');
 exports.Percolator = require(dir + 'Percolator');
--- simplest.js
+++ simplest.js
@@ -1,9 +1,11 @@
-var Percolator = require('./percolator');
+var Percolator = require('./index').Percolator;
 
 var server = new Percolator();
-server.route('/', {  GET : function($){
-                              $.res.end("Hello World!");
-                            }});
+server.route('/', function($){
+                  $.json({hello : "world!"})
+                   .send();
+                  });
 server.listen(function(err){
-  console.log('server is listening on port ', server.port);
+	if (err) { throw err; }
+	console.log("Percolator is listening on port ", server.port);	
 });
