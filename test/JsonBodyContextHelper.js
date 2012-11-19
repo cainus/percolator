e7fc46966bc086e772ffc4b44f2ea8076e0169c1
Gregg Caines <gcaines@exacttarget.com>
2012-11-19 11:36:57 -0800

    got JsonModule to use $.onJson

--- test/JsonBodyContextHelper.js
+++ test/JsonBodyContextHelper.js
@@ -11,6 +11,20 @@ describe("JsonBodyContextHelper", function(){
       done();
     });
   });
+  it ("throws an error when called with 2+ params", function(done){
+    var handler = {};
+    var $ = { };
+    jbch($, handler, function(){
+      try {
+        $.onJson({}, {}, function(err, obj){
+          err.should.equal('some error');
+        });
+      } catch(ex){
+        ex.should.equal('$.onJson() was called with the wrong number of properties.');
+        done();
+      }
+    });
+  });
   it ("sets the error param when there's an error", function(done){
     var fakeReq = {
       on : function(type, cb){
@@ -44,6 +58,117 @@ describe("JsonBodyContextHelper", function(){
       $.onJson(function(err, obj){
         obj.should.eql({asdf:"asdf"});
         done();
+      });
+    });
+  });
+  it ("responds with an error if json doesn't parse", function(done){
+    var fakeReq = {
+      on : function(type, cb){
+        switch(type){
+          case 'data' : return cb('{"age":37,}');
+          case 'end' : return cb();
+        }
+      }
+    };
+    var handler = {};
+    var $ = { req : fakeReq,
+              status : {
+                badRequest : function(message, detail){
+                  message.should.equal('invalid json.');
+                  detail.should.equal('{"age":37,}');
+                  done();
+                }
+              }
+             };
+    jbch($, handler, function(){
+      $.onJson(function(err, obj){
+        should.fail('should not get here');
+      });
+    });
+  });
+  it ("responds with an error if input incorrectly has additional properties", function(done){
+    var schema = {
+             "properties": {
+                "age": {
+                  "type": "number"
+                },
+                "name": {
+                  "type": "string"
+                }
+              },
+              "additionalProperties" : false
+            };
+    var fakeReq = {
+      on : function(type, cb){
+        switch(type){
+          case 'data' : return cb('{"age":37, "name":"GDizzle", "wrong" : "wrong"}');
+                          // above mistakenly sends an additional property when
+                          // additionalProperties is false
+          case 'end' : return cb();
+        }
+      }
+    };
+    var handler = {};
+    var $ = { req : fakeReq,
+              status : {
+                badRequest : function(message, detail){
+                  message.should.equal('json failed schema validation.');
+                  detail[0].message.should.equal('Additional properties are not allowed');
+                  // detail looks like this:
+                  // [ { uri: 'urn:uuid:167293d9-3c95-493d-826e-1bfd4146a8b9#',
+                  // schemaUri: 'urn:uuid:b7e07efd-fd80-4370-8206-9162f4c39cc9#',
+                  // attribute: 'additionalProperties',
+                  // message: 'Additional properties are not allowed',
+                  // details: false } ]
+                  done();
+                }
+              }
+             };
+    jbch($, handler, function(){
+      $.onJson(schema, function(err, obj){
+        should.fail('should not get here');
+      });
+    });
+  });
+  it ("throws errors when input doesn't match schema", function(done){
+    var schema = {
+             "properties": {
+                "age": {
+                  "type": "number"
+                },
+                "name": {
+                  "type": "string"
+                }
+              }
+            };
+    var fakeReq = {
+      on : function(type, cb){
+        switch(type){
+          case 'data' : return cb('{"age":"37", "name":"GDizzle"}');
+                        // mistakenly send age as a string
+          case 'end' : return cb();
+        }
+      }
+    };
+    var handler = {};
+    var $ = { req : fakeReq,
+              status : {
+                badRequest : function(message, detail){
+                  message.should.equal('json failed schema validation.');
+                  detail[0].details[0].should.equal('number');
+                  // detail looks like this:
+                  // [ { uri: 'urn:uuid:67ef53a9-1b09-48b1-b97d-fae313e4ee39#/age',
+                  // schemaUri: 'urn:uuid:51edca59-120a-4486-a961-0ee2aa5c276b#/properties/age',
+                  // attribute: 'type',
+                  // message: 'Instance is not a required type',
+                  // details: [ 'number' ] } ]
+                  done();
+                }
+              }
+             };
+    jbch($, handler, function(){
+      $.onJson(schema, function(err, obj){
+        should.fail('should not get here');
       });
     });
   });
--- test/JsonModule.js
+++ test/JsonModule.js
@@ -2,41 +2,28 @@ var JsonModule = require('../index').JsonModule;
 var should = require('should');
 
 describe("JsonModule", function(){
-    it ("throws an exception if there's no list() passed in the options param", function(){
-      try {
-        var module = new JsonModule({});
-        should.fail("expected exception was not raised");
-      } catch(ex){
-        ex.should.equal("JsonModule needs an options array with a 'list' function.");
-      }
+  it ("throws an exception if there's no list() passed in the options param", function(){
+    try {
+      var module = new JsonModule({});
+      should.fail("expected exception was not raised");
+    } catch(ex){
+      ex.should.equal("JsonModule needs an options array with a 'list' function.");
+    }
 
-    });
-    it ("throws an error if upsert() and update() both exist", function(){
-      try {
-        var module = new JsonModule({
-                                      list : function($, cb){ cb([]); },
-                                      update : function($, id, obj){ },
-                                      upsert : function($, id, obj){ }
-                                     });
-        should.fail("expected exception was not raised");
-      } catch (ex){
-        ex.should.equal("JsonModule should not have create() or update() if it has upsert().");
-      }
+  });
+  it ("throws an error if upsert() and update() both exist", function(){
+    try {
+      var module = new JsonModule({
+                                    list : function($, cb){ cb([]); },
+                                    update : function($, id, obj){ },
+                                    upsert : function($, id, obj){ }
+                                   });
+      should.fail("expected exception was not raised");
+    } catch (ex){
+      ex.should.equal("JsonModule should not have create() or update() if it has upsert().");
+    }
 
-    });
-    it ("throws an error if upsert() and create() both exist", function(){
-      try {
-        var module = new JsonModule({
-                                      list : function($, cb){ cb([]); },
-                                      create : function($, id, obj){ },
-                                      upsert : function($, id, obj){ }
-                                     });
-        should.fail("expected exception was not raised");
-      } catch (ex){
-        ex.should.equal("JsonModule should not have create() or update() if it has upsert().");
-      }
-
-    });
+  });
   describe("when schema is set", function(){
     it ("collection GET outputs a create link", function(done){
         var module = new JsonModule({
@@ -143,7 +130,11 @@ describe("JsonModule", function(){
       should.not.exist(module.wildcard.PUT);
     });
     it ("calls options.upsert() if it exists", function(done){
+      var schema = {
+        "name" : "name"
+      };
       var module = new JsonModule({
+                                    schema : schema,
                                     list : function($, cb){ cb([]); },
                                     upsert : function($, id, obj){ 
                                       obj.should.eql({age:37});
@@ -156,18 +147,25 @@ describe("JsonModule", function(){
             return 'http://collection/1234';
           }
         },
-        onBody : function(cb){
-          cb(null, '{"age":37}');
+        onJson : function(schema, cb){
+          schema.should.eql(schema);
+          cb(null, {"age":37});
         }
       };
       module.wildcard.PUT($);
     });
-    it ("calls options.update() if it exists", function(done){
+    it ("calls options.upsert() with its callback if it exists", function(done){
+      var headerSet = false;
+      var headWritten = false;
+      var schema = {
+        "name" : "name"
+      };
       var module = new JsonModule({
+                                    schema : schema,
                                     list : function($, cb){ cb([]); },
-                                    update : function($, id, obj){ 
+                                    upsert : function($, id, obj, cb){ 
                                       obj.should.eql({age:37});
-                                      done();
+                                      cb();
                                     }
                                   });
       var $ = {
@@ -176,75 +174,28 @@ describe("JsonModule", function(){
             return 'http://collection/1234';
           }
         },
-        onBody : function(cb){
-          cb(null, '{"age":37}');
+        res : {
+          setHeader : function(name, value){
+            headerSet = true;
+            name.should.equal('Location');
+            value.should.equal('http://collection/1234');
+          },
+          writeHead : function(code){
+            headWritten = true;
+            code.should.equal(303);
+          },
+          end : function(){
+            headerSet.should.equal(true);
+            headWritten.should.equal(true);
+            done();
+          }
+        },
+        onJson : function(schema, cb){
+          schema.should.eql(schema);
+          cb(null, {"age":37});
         }
       };
       module.wildcard.PUT($);
-    });
-    it ("throws an error when the json is invalid", function(done){
-          var module = new JsonModule({
-                                        list : function($, cb){ cb([]); },
-                                        update : function($, id, obj){ }
-                                      });
-          var $ = {
-            status : {
-              badRequest : function(message, detail){
-                message.should.equal('invalid json.');
-                detail.should.equal('{"age":37,}');
-                done();
-              }
-            },
-
-            onBody : function(cb){
-              cb(null, '{"age":37,}');
-              // mistakenly sent invalid json
-            }
-
-          };
-          module.wildcard.PUT($);
-    });
-    it ("throws errors when input doesn't match schema", function(done){
-          var module = new JsonModule({
-                                        list : function($, cb){ cb([]); },
-                                        update : function($, id, obj){ },
-                                        schema : {
-                                                   "properties": {
-                                                      "age": {
-                                                        "type": "number"
-                                                      },
-                                                      "name": {
-                                                        "type": "string"
-                                                      }
-                                                    }
-                                                  }
-                                      });
-          var $ = {
-            status : {
-              badRequest : function(message, detail){
-                message.should.equal('json failed schema validation.');
-                detail[0].details[0].should.equal('number');
-                // detail looks like this:
-                // [ { uri: 'urn:uuid:67ef53a9-1b09-48b1-b97d-fae313e4ee39#/age',
-                // schemaUri: 'urn:uuid:51edca59-120a-4486-a961-0ee2aa5c276b#/properties/age',
-                // attribute: 'type',
-                // message: 'Instance is not a required type',
-                // details: [ 'number' ] } ]
-                done();
-              }
-            },
-            uri : {
-              self : function(){
-                return 'http://collection/1234';
-              }
-            },
-            onBody : function(cb){
-              cb(null, '{"age":"37", "name":"GDizzle"}'); 
-              // above mistakenly sends age as a string
-            }
-
-          };
-          module.wildcard.PUT($);
     });
     it ("calls options.update()", function(done){
       var module = new JsonModule({
@@ -261,8 +212,9 @@ describe("JsonModule", function(){
             return 'http://collection/1234';
           }
         },
-        onBody : function(cb){
-          cb(null, '{"age":37}');
+        onJson : function(schema, cb){
+          // TODO: verify schema
+          cb(null, {"age":37});
         }
       };
       module.wildcard.PUT($);
@@ -290,6 +242,8 @@ describe("JsonModule", function(){
             code.should.equal(303);
           },
           end : function(){
+            headerSet.should.equal(true);
+            headWritten.should.equal(true);
             done();
           }
         },
@@ -298,117 +252,15 @@ describe("JsonModule", function(){
             return 'http://collection/1234';
           }
         },
-        onBody : function(cb){
-          cb(null, '{"age":37}');
+        onJson : function(schema, cb){
+          //TODO verify schema
+          cb(null, {"age":37});
         }
       };
       module.wildcard.PUT($);
     });
   });
   describe("handler.POST", function(){
-    it ("throws errors when input doesn't match schema", function(done){
-          var module = new JsonModule({
-                                        list : function($, cb){ cb([]); },
-                                        create : function($, obj){ },
-                                        schema : {
-                                                   "properties": {
-                                                      "age": {
-                                                        "type": "number"
-                                                      },
-                                                      "name": {
-                                                        "type": "string"
-                                                      }
-                                                    }
-                                                  }
-                                      });
-          var $ = {
-            status : {
-              badRequest : function(message, detail){
-                message.should.equal('json failed schema validation.');
-                detail[0].details[0].should.equal('number');
-                // detail looks like this:
-                // [ { uri: 'urn:uuid:67ef53a9-1b09-48b1-b97d-fae313e4ee39#/age',
-                // schemaUri: 'urn:uuid:51edca59-120a-4486-a961-0ee2aa5c276b#/properties/age',
-                // attribute: 'type',
-                // message: 'Instance is not a required type',
-                // details: [ 'number' ] } ]
-                done();
-              }
-            },
-
-            onBody : function(cb){
-              cb(null, '{"age":"37", "name":"GDizzle"}'); 
-              // above mistakenly sends age as a string
-            }
-
-          };
-          module.handler.POST($);
-    });
-
-    it ("throws errors when input incorrectly has additional properties", function(done){
-          var module = new JsonModule({
-                                        list : function($, cb){ cb([]); },
-                                        create : function($, obj){ },
-                                        schema : {
-                                                   "properties": {
-                                                      "age": {
-                                                        "type": "number"
-                                                      },
-                                                      "name": {
-                                                        "type": "string"
-                                                      }
-                                                    },
-                                                    "additionalProperties" : false
-                                                  }
-                                      });
-          var $ = {
-            status : {
-              badRequest : function(message, detail){
-                message.should.equal('json failed schema validation.');
-                detail[0].message.should.equal('Additional properties are not allowed');
-                // detail looks like this:
-                // [ { uri: 'urn:uuid:167293d9-3c95-493d-826e-1bfd4146a8b9#',
-                // schemaUri: 'urn:uuid:b7e07efd-fd80-4370-8206-9162f4c39cc9#',
-                // attribute: 'additionalProperties',
-                // message: 'Additional properties are not allowed',
-                // details: false } ]
-                done();
-              }
-            },
-
-            onBody : function(cb){
-              cb(null, '{"age":37, "name":"GDizzle", "wrong" : "wrong"}'); 
-              // above mistakenly sends an additional property when
-              // additionalProperties is false
-            }
-
-          };
-          module.handler.POST($);
-    });
-
-    it ("throws an error when the json is invalid", function(done){
-          var module = new JsonModule({
-                                        list : function($, cb){ cb([]); },
-                                        create : function($, obj){ }
-                                      });
-          var $ = {
-            status : {
-              badRequest : function(message, detail){
-                message.should.equal('invalid json.');
-                detail.should.equal('{"age":37,}');
-                done();
-              }
-            },
-
-            onBody : function(cb){
-              cb(null, '{"age":37,}');
-              // mistakenly sent invalid json
-            }
-
-          };
-          module.handler.POST($);
-    });
-
     it ("doesn't exist if options has no create()", function(){
       // the router will 405 a collection POST if the handler doesn't 
       // implement .POST()
@@ -428,8 +280,9 @@ describe("JsonModule", function(){
                                      }
                                   });
       var $ = {
-        onBody : function(cb){
-          cb(null, '{"age":37}');
+        onJson : function(schema, cb){
+          //TODO: verify schema
+          cb(null, {"age":37});
         }
       };
       module.handler.POST($);
@@ -454,8 +307,9 @@ describe("JsonModule", function(){
             return 'self';
           }
         },
-        onBody : function(cb){
-          cb(null, '{"age":37}');
+        onJson : function(schema, cb){
+          //TODO: verify schema
+          cb(null, {"age":37});
         }
       };
       module.handler.POST($);
