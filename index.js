bb0e9fbfd914c4ee5ef5975ae4051dff9fafa434
cainus <gregg@caines.ca>
2013-05-27 20:40:40 -0700

    extracted onbody middleware.

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ exports.CRUDCollection = require(dir + 'CRUDCollection');
 exports.AuthenticateHelper = require(helperDir + 'Authenticate');
 exports.BasicAuthenticateHelper = require(helperDir + 'BasicAuthenticate');
 exports.ObjectHelper = require(helperDir + 'Object');
-exports.onBodyHelper = require(helperDir + 'onBody');
 exports.onJsonHelper = require(helperDir + 'onJson');
 exports.FetchHelper = require(helperDir + 'Fetch');
 exports.ContextFaker = require(dir + 'ContextFaker');
